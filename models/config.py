5b6484b0026008e02d66b616157719f3f160c016
Omur Ozbahceliler <omur@rootifera.org>
2024-12-11 22:59:01 +0000

    more validations

--- models/config.py
+++ models/config.py
@@ -1,9 +1,9 @@
 from sqlmodel import Field, SQLModel, Relationship
 from typing import Optional, List
 
-
 class Config(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
 
     cpu_id: Optional[int] = Field(default=None, foreign_key="cpu.id")
     motherboard_id: Optional[int] = Field(default=None, foreign_key="motherboard.id")
--- routers/config.py
+++ routers/config.py
@@ -3,6 +3,11 @@ from sqlmodel import Session, select
 from utils.helper import validate_and_normalize_name
 from models.config import Config
 from models.ram import RAM
+from models.cpu import CPU
+from models.motherboard import Motherboard
+from models.gpu import GPU
+from models.disk import Disk
+from models.oses import OS
 from database import get_db
 
 router = APIRouter()
@@ -13,8 +18,7 @@ def create_config(config: Config, db: Session = Depends(get_db)):
     if ram_type is None:
         raise HTTPException(status_code=400, detail="Invalid RAM type")
 
-    if hasattr(config, "name"):
-        config.name = validate_and_normalize_name(config.name, db, Config)
+    config.name = validate_and_normalize_name(config.name, db, Config)
 
     db.add(config)
     db.commit()
@@ -39,6 +43,26 @@ def update_config(config_id: int, config: Config, db: Session = Depends(get_db))
     if ram_type is None:
         raise HTTPException(status_code=400, detail="Invalid RAM type")
 
+    cpu = db.get(CPU, config.cpu_id)
+    if cpu is None:
+        raise HTTPException(status_code=400, detail="Invalid CPU")
+
+    motherboard = db.get(Motherboard, config.motherboard_id)
+    if motherboard is None:
+        raise HTTPException(status_code=400, detail="Invalid Motherboard")
+
+    gpu = db.get(GPU, config.gpu_id)
+    if gpu is None:
+        raise HTTPException(status_code=400, detail="Invalid GPU")
+
+    disk = db.get(Disk, config.disk_id)
+    if disk is None:
+        raise HTTPException(status_code=400, detail="Invalid Disk")
+
+    os = db.get(OS, config.os_id)
+    if os is None:
+        raise HTTPException(status_code=400, detail="Invalid OS")
+
     db_config = db.get(Config, config_id)
     if db_config is None:
         raise HTTPException(status_code=404, detail="Config not found")
@@ -46,6 +70,7 @@ def update_config(config_id: int, config: Config, db: Session = Depends(get_db))
     if hasattr(config, "name"):
         config.name = validate_and_normalize_name(config.name, db, Config)
 
+    db_config.name = config.name
     db_config.cpu_id = config.cpu_id
     db_config.motherboard_id = config.motherboard_id
     db_config.gpu_id = config.gpu_id
