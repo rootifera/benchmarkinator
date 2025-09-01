35ca6193f828c4487c6655f8e51ba0e1a168fd74
Omur Ozbahceliler <omur@rootifera.org>
2025-09-01 16:00:30 +0100

    lower is better added

--- models/benchmark.py
+++ models/benchmark.py
@@ -1,5 +1,8 @@
+from typing import List, Optional, TYPE_CHECKING
 from sqlmodel import Field, SQLModel, Relationship
-from typing import List, Optional
+
+if TYPE_CHECKING:
+    from models.benchmark_results import BenchmarkResult
 
 class BenchmarkTarget(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -17,16 +20,5 @@ class Benchmark(SQLModel, table=True):
     benchmark_target_id: Optional[int] = Field(default=None, foreign_key="benchmarktarget.id")
 
     target: Optional[BenchmarkTarget] = Relationship(back_populates="benchmarks")
-    benchmark_results: List["Benchmarks"] = Relationship(back_populates="benchmark")
 
-
-class Benchmarks(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-
-    config_id: Optional[int] = Field(default=None, foreign_key="config.id")
-    benchmark_id: Optional[int] = Field(default=None, foreign_key="benchmark.id")
-
-    result: Optional[str] = None
-
-    config: Optional["Config"] = Relationship()
-    benchmark: Optional["Benchmark"] = Relationship()
+    benchmark_results: List["BenchmarkResult"] = Relationship(back_populates="benchmark")
