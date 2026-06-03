e0a7e95e5bd1f467286adda59e4beb312a96170d
Omur Ozbahceliler <omur@rootifera.org>
2026-06-03 22:12:07 +0100

    minor change to vite

--- webui/vite.config.js
+++ webui/vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_DEV_API_TARGET || 'http://localhost:12345',
+        changeOrigin: true,
+      },
+    },
+  },
 });
